07080d477213aeaef8544faf72b88747ec45417b
kushagrachoudhary <kushagrachoudhary60@gmail.com>
2024-08-27 11:53:01 +0530

    add folder

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-black">
-      <header className='text-white '>hellow</header>
+    <div>
+      
     </div>
   );
 }
---
+++ src/Header/Head.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <div className='w-100% h-56 bg-white '>Head</div>
+  )
+}
