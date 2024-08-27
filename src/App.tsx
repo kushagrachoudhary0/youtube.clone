1497f9f84e1761570019b80b2fde9a2fbf2d7918
Tanish Saini <tanish.saini@deeporion.com>
2024-08-27 14:49:57 +0530

    create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 // import './App.css';
 import Head from './Header/Head.tsx';
+import Sidebar from './Comp/Sidebar.tsx';
+import '../src/Comp/Api.js'
 
 function App() {
   return (
     <div>
      <Head/>
+     <Sidebar/>
     </div>
   );
 }
