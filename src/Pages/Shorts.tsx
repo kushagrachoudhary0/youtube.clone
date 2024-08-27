063a76cc435fb13db56becf93399ae5c673c1796
kushagrachoudhary <kushagrachoudhary60@gmail.com>
2024-08-27 16:49:16 +0530

    add routing

---
+++ src/Pages/Shorts.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Head from '../Header/Head.tsx'
+import Sidebar from '../Comp/Sidebar.tsx'
+
+export default class Shorts extends Component {
+  render() {
+    return (
+      <div>
+        <Head/>
+      </div>
+    )
+  }
+}
